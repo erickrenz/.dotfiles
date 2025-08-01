25e1db9047abb5c9fe11764f8ac625e13e634784
erickrenz <krenz.eric.krenz@gmail.com>
2025-08-01 07:58:52 -0400

    feat: update nvim keysets and remap docs

--- nvim/lua/custom/set.lua
+++ nvim/lua/custom/set.lua
@@ -2,43 +2,35 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.g.netrw_banner = 0
 
-vim.o.hlsearch = false
 vim.o.number = true
-vim.o.nu = true
 vim.o.relativenumber = true
-vim.o.scrolloff = 8
-vim.o.mouse = 'a'
-vim.o.showmode = false
 
-vim.o.clipboard = 'unnamedplus'
+vim.schedule(function()
+  vim.o.clipboard = 'unnamedplus'
+end)
 
-vim.o.breakindent = true
+vim.o.hlsearch = false
 vim.o.ignorecase = true
 vim.o.smartcase = true
-
-vim.wo.signcolumn = 'yes'
-
-vim.o.updatetime = 250
-vim.o.timeout = true
-vim.o.timeoutlen = 300
-
-vim.o.completeopt = 'menuone,noselect'
-vim.o.termguicolors = true
 
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
 
+vim.o.breakindent = true
 vim.o.smartindent = true
 vim.o.wrap = false
 
 vim.o.swapfile = false
-vim.o.backup = false
 vim.o.undofile = true
 
-vim.o.updatetime = 50
+vim.o.scrolloff = 8
 vim.o.colorcolumn = '80'
-
+vim.o.signcolumn = 'yes'
 vim.o.inccommand = 'split'
+
+vim.o.mouse = 'a'
 vim.o.confirm = true
+vim.o.showmode = false
+vim.o.termguicolors = true
