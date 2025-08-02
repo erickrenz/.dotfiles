a2bdd7e4eeb082ec6aa2c73b2db61cc6aa8ba564
erickrenz <krenz.eric.krenz@gmail.com>
2025-08-02 07:10:10 -0400

    feat: add nvim indent-blankline

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -1,6 +1,8 @@
 return {
   { 'NMAC427/guess-indent.nvim', opts = {} },
 
+  { 'lukas-reineke/indent-blankline.nvim', main = 'ibl', opts = {} },
+
   { 'lewis6991/gitsigns.nvim', opts = {} },
 
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
